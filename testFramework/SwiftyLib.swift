ed1c5351b179685aaf0802166b4cd7c4d46771f6
SonalKachare <sonal@clevertap.com>
2022-01-18 19:11:51 +0530

    added framework file

---
+++ testFramework/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  testFramework
+//
+//  Created by Sonal Kachare on 18/01/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
